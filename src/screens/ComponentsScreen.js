d51a41deab77e94c9fbc91dd4c7eed3cb40bf053
emrekas <y.emrekas@outlook.com>
2021-01-03 18:02:15 +0300

    Component eklendi ve kullanildi

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const ComponentsScreen = () => {
+  return (
+    <Text style={styles.text}>This is the components screen</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default ComponentsScreen;
